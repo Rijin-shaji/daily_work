ef2bdfd82ed365740f2bad8b1595256e4e5925ba
rijinshaji <rijinshajiedathil@gmail.com>
2026-03-28 21:41:23 +0530

    made changes

--- Hr_hire_candidates.py
+++ Hr_hire_candidates.py
@@ -2,15 +2,12 @@ from Hr_resume_status import mark_as_hired, load_status
 
 def main():
     resume_filename = input("Enter resume filename to mark as hired: ").strip()
-
     status_data = load_status()
 
     if resume_filename in status_data and status_data[resume_filename]["status"] == "hired":
         print(f"{resume_filename} is already marked as hired.")
         return
-
     mark_as_hired(resume_filename)
     print(f"{resume_filename} successfully marked as hired.")
-
 if __name__ == "__main__":
     main()
